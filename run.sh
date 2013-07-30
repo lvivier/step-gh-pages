67e0a4af69e5612e5e8343c42851c93371bc61db
Luke Vivier <luke@vivier.ca>
2013-07-29 18:23:08 -0700

    first commit

---
+++ run.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# confirm environment variables
+if [ ! -n "$WERCKER_GH_PAGES_TOKEN" ]
+then
+  fail "missing option \"token\", aborting"
+fi
+
+if [ -n "$WERKCER_GH_PAGES_PROJECT" ]
+then
+  remote="https://$WERCKER_GH_PAGES_TOKEN@github.com/$WERCKER_GH_PAGES_PROJECT.git"
+else
+  remote="https://$WERCKER_GH_PAGES_TOKEN@github.com/$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY.git"
+fi
+
+# if directory provided, cd to it
+if [ -f "$WERCKER_OUTPUT_DIR/$WERCKER_GH_PAGES_BASEDIR" ]
+then
+  cd $WERCKER_OUTPUT_DIR/$WERCKER_GH_PAGES_BASEDIR
+else
+  cd $WERCKER_OUTPUT_DIR
+fi
+
+# remove existing commit history
+rm -rf .git
+
+# generate cname file
+if [ -n $WERCKER_GH_PAGES_DOMAIN ]
+then
+  echo $WERCKER_GH_PAGES_DOMAIN > CNAME
+fi
+
+# init repository
+git init
+git add .
+git commit -m "deploy from $WERCKER_STARTED_BY"
+git push -f $remote master:gh-pages
+
+success "pushed to github pages"
