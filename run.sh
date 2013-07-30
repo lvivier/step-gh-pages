4b38b5870dddb9f393c84c9997d9454f8ef0398d
Luke Vivier <luke@vivier.ca>
2013-07-29 19:19:56 -0700

    add git config lines, better fail

--- run.sh
+++ run.sh
@@ -32,8 +32,19 @@ fi
 
 # init repository
 git init
+
+git config user.email "pleasemailus@wercker.com"
+git config user.name "werckerbot"
+
 git add .
 git commit -m "deploy from $WERCKER_STARTED_BY"
-git push -f $remote master:gh-pages
+result="$(git push -f $remote master:gh-pages)"
 
-success "pushed to github pages"
+if [[ $? -ne 0 ]]
+then
+  warning "$result"
+  fail "failed pushing to github pages"
+else
+  success "pushed to github pages"
+fi
+
