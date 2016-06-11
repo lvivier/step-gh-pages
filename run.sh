28d85eedbe1bd24dd9c8c18fab6eec065fbf0d01
Gerard Lynch <gerard@halberom.co.uk>
2016-06-11 14:59:31 +0100

    fix shellcheck posix error

--- run.sh
+++ run.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
 # confirm environment variables
-if [ ! -n "$WERCKER_GH_PAGES_TOKEN" ]
-then
+if [ ! -n "$WERCKER_GH_PAGES_TOKEN" ]; then
   fail "missing option \"token\", aborting"
 fi
 
 # use repo option or guess from git info
-if [ -n "$WERCKER_GH_PAGES_REPO" ]
-then
+if [ -n "$WERCKER_GH_PAGES_REPO" ]; then
   repo="$WERCKER_GH_PAGES_REPO"
-elif [ 'github.com' == "$WERCKER_GIT_DOMAIN" ]
-then
+elif [ 'github.com' = "$WERCKER_GIT_DOMAIN" ]; then
   repo="$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY"
 else
   fail "missing option \"repo\", aborting"
@@ -23,8 +20,7 @@ info "using github repo \"$repo\""
 remote="https://$WERCKER_GH_PAGES_TOKEN@github.com/$repo.git"
 
 # if directory provided, cd to it
-if [ -d "$WERCKER_GH_PAGES_BASEDIR" ]
-then
+if [ -d "$WERCKER_GH_PAGES_BASEDIR" ]; then
   cd $WERCKER_GH_PAGES_BASEDIR
 fi
 
@@ -32,15 +28,14 @@ fi
 rm -rf .git
 
 # generate cname file
-if [ -n "$WERCKER_GH_PAGES_DOMAIN" ]
-then
+if [ -n "$WERCKER_GH_PAGES_DOMAIN" ]; then
   echo $WERCKER_GH_PAGES_DOMAIN > CNAME
 fi
 
 # setup branch
 branch="gh-pages"
-if echo $repo | grep -qE "$WERCKER_GIT_OWNER\/$WERCKER_GIT_OWNER\.github\.(io|com)"
-then
+echo $repo | grep -qE "$WERCKER_GIT_OWNER\/$WERCKER_GIT_OWNER\.github\.(io|com)"
+if [ $? -eq 0 ]; then
   branch="master"
 fi
 
@@ -54,8 +49,7 @@ git add .
 git commit -m "deploy from $WERCKER_STARTED_BY"
 result="$(git push -f $remote master:$branch)"
 
-if [ $? -ne 0 ]
-then
+if [ $? -ne 0 ]; then
   warning "$result"
   fail "failed pushing to github pages"
 else
